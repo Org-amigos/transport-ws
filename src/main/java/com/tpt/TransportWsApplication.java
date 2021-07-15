85e23186c05cffce88c0ebedd80c76cfaaa506a7
shaik sajid <shaiksajid492@gmail.com>
2021-07-16 01:27:51 +0530

    2 nd commit

--- src/main/java/com/tpt/TransportWsApplication.java
+++ src/main/java/com/tpt/TransportWsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.tpt.*")
-@EnableAutoConfigurationewt
+@EnableAutoConfiguration
 public class TransportWsApplication {
 
 	public static void main(String[] args) {
